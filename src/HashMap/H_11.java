7f025952f0b37c4f76433ed093f9fc1dcda356e9
Shreyansh1912 <98804905+Shreyansh1912@users.noreply.github.com>
2022-06-03 17:02:42 +0530

    Longest Sub-Array with Sum K

---
+++ src/HashMap/H_11.java
@@ -0,0 +1,67 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    int K = Integer.parseInt(element[1]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    int res = obj.lenOfLongSubarr(arr, sizeOfArray, K);
+		    
+		    System.out.println(res);
+		}
+	}
+}
+
+// } Driver Code Ends
+
+class H_11{
+    public int lenOfLongSubarr (int nums[], int N, int k) {
+        int answer = 0;
+        int prefixSum = 0;
+        
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        memo.put(prefixSum, -1);
+        
+        for(int i=0; i<N; i++){
+            prefixSum += nums[i];
+            
+            if(memo.containsKey(prefixSum-k))
+                answer = Math.max(answer,  i - memo.get(prefixSum-k));
+    
+            if(!memo.containsKey(prefixSum))
+                memo.put(prefixSum, i);
+        }
+        return answer;
+    }
+    
+    
+}
+
+
