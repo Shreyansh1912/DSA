4d973489bada451971045b0d28f1cd8e6be2dab3
Shreyansh <aayansrivastava1912@gmail.com>
2022-06-11 02:31:11 +0530

    Search in a Binary Search Tree using recursive approach

---
+++ src/Binary_Search_Tree/BST_01.java
@@ -0,0 +1,26 @@
+package Binary_Search_Tree;
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+public class BST_01{
+    public TreeNode searchBST(TreeNode root, int val) {
+
+        if(root == null) return null;
+
+        if(root.val == val) return root;
+
+        if(root.val > val) return searchBST(root.left, val);
+
+        return searchBST(root.right, val);            // TC : o(logN)
+    }
+}
