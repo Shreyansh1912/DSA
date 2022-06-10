4d59f526bec8abac7a405cb14e06122ed16bc8c4
Shreyansh1912 <98804905+Shreyansh1912@users.noreply.github.com>
2022-06-11 02:38:02 +0530

    Search in a Binary Search Tree using Iterative way

---
+++ src/Binary_Search_Tree/BST_02.java
@@ -0,0 +1,32 @@
+public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ 
+ class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+       
+        TreeNode current = root;
+        
+        while(current != null){
+            
+            if(current.val == val)
+                return current;
+            
+            if(current.val > val)
+                current = current.left;
+            
+            else
+                current = current.right;
+        }
+        return null;                                    
+    }
+}
