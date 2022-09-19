bf5069d2f23f73464b10a13191247e8cb8fea8ae
Shreyansh1912 <98804905+Shreyansh1912@users.noreply.github.com>
2022-09-20 03:30:15 +0530

    Remove Duplicates from Sorted List II

---
+++ src/LinkedList/LL_33.java
@@ -0,0 +1,33 @@
+public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        //BruteForce Approach --> HashMap--> TC:O(2N), SC:O(2N)
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        
+        ListNode prevNode = dummyNode;
+        ListNode currentNode = head;
+        
+        while(currentNode != null){
+            boolean flag = false;
+            
+            while(currentNode.next != null && currentNode.next.val == currentNode.val){
+                flag = true;
+                currentNode = currentNode.next;
+            }
+            if(flag)
+                prevNode.next = currentNode.next;
+            else 
+                prevNode = currentNode;
+            
+            currentNode = currentNode.next;
+        }
+        return dummyNode.next;
+    }
+}
