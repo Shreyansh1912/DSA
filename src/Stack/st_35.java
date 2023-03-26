e7b253e1e4309d7757f636a7ac10e9b631fbbc76
Shreyansh1912 <98804905+Shreyansh1912@users.noreply.github.com>
2023-03-26 23:16:06 +0530

    Postfix To Prefix conversion

---
+++ src/Stack/st_35.java
@@ -0,0 +1,20 @@
+public String postfixToPrefix(String postfix){   //TC: O(N), SC: O(N)
+        Stack<String> stack = new Stack<>();
+
+        for(int i=0; i<postfix.length(); i++){
+            Character currentChar = postfix.charAt(i);
+
+            if(isOperator(currentChar)){
+               String firstVal = stack.pop();
+               String secondVal = stack.pop();
+
+               stack.push(currentChar+ secondVal +  firstVal);
+            } else{
+                stack.push(currentChar + "");
+            }
+        }
+        return stack.peek();
+    }
+private boolean isOperator(Character currentChar) {
+        return currentChar == '+' || currentChar == '-' || currentChar == '*' || currentChar == '/' || currentChar == '^';
+    }
